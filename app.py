2db6e63e83dd1db9a79cdd3639877820659fb501
= <=>
2026-01-28 16:38:35 +0200

    Added login functionality to app.py

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
+import sqlite3
 from flask import Flask
+from flask import redirect, render_template, request, session
+import config, forum, users
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    threads = forum.get_threads()
+    return render_template("index.html", threads=threads)
+
+
+#sovellus
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if password1 != password2:
+            return render_template("register_unsuccesful.html", message="salasanat eivät täsmää")
+
+        try:
+            users.create_user(username, password1)
+            return render_template("register_succesful.html")
+        except sqlite3.IntegrityError:
+            return render_template("register_unsuccesful.html", message="käyttäjänimi on jo varattu")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            return redirect("/")
+        else:
+            return render_template("login_unsuccesful.html")
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    return redirect("/")
